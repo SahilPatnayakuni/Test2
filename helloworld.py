ac532445f11894b63dcaa21e6fe71691861b6d19
Sahil Patnayakuni <sahil.patnayakuni@gmail.com>
2021-10-06 10:49:53 -0500

    New PR for testing

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,9 @@
 print("Hello World")
 print("Goodbye World")
-print("This String has no end Quote")
+print("This String has no end Quote)
+
+dict_var = {'key1': 'value1', 'key2': 'value2'}
+print(dict_var)
+
+print(dict_var.get('some_key', None))
+print(dict_var.get('wrong-parens', None)
