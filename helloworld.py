88935eaac80b1cac2852d1c1dc37feaa42b576ac
Sahil Patnayakuni <sahil.patnayakuni@gmail.com>
2021-11-10 10:14:57 -0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,7 @@ print("This String has no end Quote)
 
 dict_var = {'key1': 'value1', 'key2': 'value2'}
 print(dict_var)
-
+print(dict_var['key1'])
 print(dict_var.get('some_key', None))
 print(dict_var.get('wrong-parens', None))
 print(dict_var.get('wrong-parens', None))
