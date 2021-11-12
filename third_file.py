ff626a2cdf291daa9703a5b169270a90a5c7a5c7
Sahil patnayakuni <sahil@DESKTOP-RJBTBUU.localdomain>
2021-11-12 15:02:48 -0600

    modified all three files

--- third_file.py
+++ third_file.py
@@ -6,5 +6,9 @@ print(dict_var.get('some_key', None))
 print(dict_var.get('asdfasdf', None)
 print(dict_var.get('sqwrasfa', None))
 print(dict_var.get('sqwefsafy', None)
+print(dict_var.get('some_key', None))
+print(dict_var.get('asdfasdf', None)
+print(dict_var.get('sqwrasfa', None))
+print(dict_var.get('sqwefsafy', None)
 if 1:
     pass
