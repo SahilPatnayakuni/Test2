12a7ce914cb2353923fa1b3da89021b8976d8c6e
Sahil patnayakuni <sahil@DESKTOP-RJBTBUU.localdomain>
2021-11-12 14:29:27 -0600

    modified file again

--- second_file.py
+++ second_file.py
@@ -3,11 +3,11 @@ print("good python code")
 print("some different python code")
 
 
-print("This statement doesn't have close quotes)
+print("This statement doesn't have close quotes")
 test_dict = {"hello": "hellovalue", "goodbye": "goodbyevaleus"}
 print(test_dict.get("hello", "hello is not in dict"))
 test_dict.get("goodbye", None)
 print(dict_var.get('wrong-parens', None))
 print(dict_var.get('wrong-parens', None))
 print(dict_var.get('some_key', None))
-print(dict_var.get('some_key', None)
+print(dict_var.get('some_key', None))
--- third_file.py
+++ third_file.py
@@ -1,5 +1,3 @@
 print("Hello World")
 print("Goodbye World")
 print("This String has no end Quote")
-dict_var = {}
-print(dict_var.get('some_key', None))
