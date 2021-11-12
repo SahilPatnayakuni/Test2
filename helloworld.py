c82694983a7ca5a97eb9349fed9efdd9696094a8
Sahil Patnayakuni <sahil.patnayakuni@gmail.com>
2021-11-12 13:50:41 -0600

    Update helloworld.py (#3)
    * Update helloworld.py
    
    * Update helloworld.py
    
    * Update helloworld.py
    
    * Update helloworld.py
    
    * Update helloworld.py
    
    * Update helloworld.py
    
    * Move Everything down one line
    
    * Update helloworld.py
    
    * Update helloworld.py
    
    * Update helloworld.py
    
    * Update helloworld.py
    
    * Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,18 @@
+
+
 print("Hello World")
 print("Goodbye World")
-print("This String has no end Quote)
+
+print("This String has no end Quote")
 
 dict_var = {'key1': 'value1', 'key2': 'value2'}
 print(dict_var)
-
+print(dict_var['key1'])
+print("=====")
 print(dict_var.get('some_key', None))
-print(dict_var.get('wrong-parens', None)
+print(dict_var.get('wrong-parens', None))
+print(dict_var.get('wrong-parens', None))
+print("=====")
+print('\n')
+print('\t')
+print(dict_var.get('key2'))
