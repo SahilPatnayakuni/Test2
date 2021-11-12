d51c86540787c132cfd9708bc00f3049fb7cea7d
Sahil Patnayakuni <sahil.patnayakuni@gmail.com>
2021-11-12 14:29:49 -0600

    added new file, modified first file (#4)
    * added new file, modified first file
    
    * modified file again
    
    * modified file again
    
    * modified file again
    
    * modified file again
    
    * modified file again
    
    Co-authored-by: Sahil patnayakuni <sahil@DESKTOP-RJBTBUU.localdomain>

---
+++ second_file.py
@@ -0,0 +1,13 @@
+print("good python code")
+print("good python code")
+print("some different python code")
+
+
+print("This statement doesn't have close quotes")
+test_dict = {"hello": "hellovalue", "goodbye": "goodbyevaleus"}
+print(test_dict.get("hello", "hello is not in dict"))
+test_dict.get("goodbye", None)
+print(dict_var.get('wrong-parens', None))
+print(dict_var.get('wrong-parens', None))
+print(dict_var.get('some_key', None))
+print(dict_var.get('some_key', None))
---
+++ third_file.py
@@ -0,0 +1,3 @@
+print("Hello World")
+print("Goodbye World")
+print("This String has no end Quote")
