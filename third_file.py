ba32e77f976daf516734e30cd1ac2c3b2c7513ac
Sahil patnayakuni <sahil@DESKTOP-RJBTBUU.localdomain>
2021-11-12 15:04:35 -0600

    modified all three files

--- third_file.py
+++ third_file.py
@@ -3,12 +3,4 @@ print("Goodbye Wor-ld")
 print("This String has no end Quote")
 dict_var = {}
 print(dict_var.get('some_key', None))
-print(dict_var.get('asdfasdf', None)
-print(dict_var.get('sqwrasfa', None))
-print(dict_var.get('sqwefsafy', None)
-print(dict_var.get('some_key', None))
-print(dict_var.get('asdfasdf', None)
-print(dict_var.get('sqwrasfa', None))
-print(dict_var.get('sqwefsafy', None)
-if 1:
-    pass
+print(dict_var.get('asdfasdf, None)
