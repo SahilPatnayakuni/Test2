9927465eaa311c89ebe8e9672051aae50af41a3f
Sahil Patnayakuni <sahil.patnayakuni@gmail.com>
2021-11-12 15:00:29 -0600

    Update third_file.py

--- third_file.py
+++ third_file.py
@@ -1,7 +1,7 @@
 print("Hello World")
 print("Goodbye World")
 print("This String has no end Quote")
-dict_var = {}
+dict_var = { }
 print(dict_var.get('some_key', None))
 print(dict_var.get('asdfasdf', None)
 print(dict_var.get('sqwrasfa', None))
