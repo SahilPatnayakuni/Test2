ae2e19acf8def7f6b0b192e210ea57a04c590dac
Sahil patnayakuni <sahil@DESKTOP-RJBTBUU.localdomain>
2021-11-12 14:23:04 -0600

    modified file again

--- second_file.py
+++ second_file.py
@@ -7,3 +7,6 @@ print("This statement doesn't have close quotes)
 test_dict = {"hello": "hellovalue", "goodbye": "goodbyevaleus"}
 print(test_dict.get("hello", "hello is not in dict"))
 test_dict.get("goodbye", None)
+print(dict_var.get('wrong-parens', None))
+print(dict_var.get('wrong-parens', None))
+print(dict_var.get('some_key', None))
