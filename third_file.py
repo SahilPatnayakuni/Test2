d350eaa7543e8b2b83df37396adf913ab033a6d2
Sahil Patnayakuni <sahil.patnayakuni@gmail.com>
2021-12-13 14:55:38 -0600

    Test review issues (#5)
    * modified all three files
    
    * modified all three files
    
    * modified all three files
    
    * modified all three files
    
    * Update third_file.py
    
    * Update third_file.py
    
    * modified all three files
    
    * modified all three files
    
    * modified all three files
    
    * modified all three files
    
    * Update second_file.py
    
    * Update third_file.py
    
    Co-authored-by: Sahil patnayakuni <sahil@DESKTOP-RJBTBUU.localdomain>

--- third_file.py
+++ third_file.py
@@ -1,3 +1,8 @@
 print("Hello World")
-print("Goodbye World")
+print("Hello World")
+print("Goodbye Wor-ld")
 print("This String has no end Quote")
+dict_var = {}
+print(dict_var.get('some_key', None))
+print(dict_var.get('asdfasdf, None)
+print("added line")
